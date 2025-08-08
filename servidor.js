6a83d2fd5ec5cccdbf9365055ced37915c80fbcf
GustaSM11 <gustasm.07@gmail.com>
2025-08-08 20:32:14 -0300

    atividade concluída

---
+++ servidor.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    
+    let caminho = './views/';
+    
+    res.setHeader("Tipo-Conteudo", "texto/html");
+    
+    switch(req.url){
+        case '/':
+            caminho = caminho + 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/servico':
+            caminho = caminho + 'servico.html';
+            res.statusCode = 200;
+            break;
+        case '/orcamento':
+            res.statusCode = 301;
+            res.setHeader('Location', '/contato');
+            break;
+        case '/contato':
+            caminho = caminho + 'contato.html';
+            res.statusCode = 200;
+            break;
+        default:
+            caminho = caminho + 'paginadeerro.html';
+            res.statusCode = 404;
+            break;
+    }
+    fs.readFile(caminho, (err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.end(data);
+        }
+    })
+
+})
+
+server.listen(3000, 'localhost', ()=>{
+    console.log('Ouvindo na porta 3000');
+})
